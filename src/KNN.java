c6956f5dae8f3e37d51b7e25e7f48dcc1f582e30
Sean Quinn <sean.quinn34@mail.dcu.ie>
2017-02-10 16:05:39 +0000

    Replace KNN.java

--- src/KNN.java
+++ src/KNN.java
@@ -1,9 +1,12 @@
+//package prediction;
 import java.io.*;
 import java.util.*;
 import java.text.DecimalFormat;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class KNN{
 
+	/*
 	private ArrayList<ArrayList<Double>> trainSet;
 	private ArrayList<ArrayList<Double>> trainColumns;	// needed to normalise values & calculate weightings
 	//private static double [] euclideanWeighting;
@@ -15,232 +18,75 @@ public class KNN{
 	private int predictionColumn;//API input value
 	private int [] splitPoints; //API input value - a list of splitPoint values in ascending order, eg, (7,14), for continuous values or just [] for dicrete
 
-	public KNN(String trainPath, String testPath, boolean partitionTrainSet, Boolean headings, int numColumns, int predictionColumn, int [] splitPoints) {
+	public KNN(String path, boolean partitionTrainSet, Boolean headings, int numColumns, int predictionColumn, int [] splitPoints) {
 		this.path = path;/////
 		this.headings = headings;
 		this.numColumns = numColumns; 
 		this.predictionColumn = predictionColumn;
 		this.splitPoints = splitPoints;
 		trainSet = new ArrayList<>();
-		trainColumn = new ArrayList<>();
+		trainColumns = new ArrayList<>();
 		df = new DecimalFormat("#.####"); // limits decimals to 4 decimal places
+		
 		for (int i = 0; i < numColumns; i++ ) { // this just instantiates the blank arraylists in columns so that readDataset works
 			ArrayList<Double> x = new ArrayList<>();
 			columns.add(x);
 		}
-	}
-
-	private void readDataset() {
-		String currentRow = "";
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			if(headings){
-				String headings = br.readLine(); // this takes the column headings out if they are there
-			}
-			while ((currentRow = br.readLine()) != null) {
-				ArrayList<Double> row = toDoubleArrayList(currentRow.split(",")); // splits string into string [] then into ArrayList<double>
-				dataset.add(row);
-				// add to columns
-				for (int i = 0; i < numColumns; i++) {
-					columns.get(i).add(row.get(i)); // e.g this will add the school attribute to the arrilist of all the school values for each row
-				}
-			}
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void readDataset() {
 		
 	}
+	*/
 	
-	/////////////////////////////////////////////////
-	private ArrayList<ArrayList<Double>> readDatasetFile(String filePath) {
-		ArrayList<ArrayList<Double>> dataset = new ArrayList<ArrayList<Double>>();
-		String currentRow = "";
-		try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
-			if(headings){
-				String headings = br.readLine(); // this takes the column headings out if they are there
-			}
-			while ((currentRow = br.readLine()) != null) {
-				ArrayList<Double> row = toDoubleArrayList(currentRow.split(",")); // splits string into string [] then into ArrayList<double>
-				dataset.add(row);
-			}
-			return dataset;
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	//////////////////////////////////////////////////////////////
 	
-	private ArrayList<ArrayList<Double>> createColumnStructure(ArrayList<ArrayList<Double>> input){
-		//for each row, add element at each index to arraylist at that index in columns
-		ArrayList<ArrayList<Double>> columns = new ArrayList<ArrayList<Double>>();
-		for (int i = 0; i < numColumns; i++ ) { // this just instantiates the blank arraylists in columns so that readDataset works
-			ArrayList<Double> x = new ArrayList<>();
-			columns.add(x);
-		}
-		for(ArrayList<Double> a: input){ //for each row
-			for(int i = 0; i < numColumns; i++){
-				columns.get(i).add(a.get(i));
-			}
-		}
-	}
+	private static void predictClassKNN(boolean partitionTrainSet, ArrayList<ArrayList<Double>> trainSet, ArrayList<ArrayList<Double>> validationSet, int k ) {
 	
-	
-	
-	
-	
-	/////////////////////////
-	
-	private static ArrayList<Double> toDoubleArrayList(String [] values) {
-		ArrayList<Double> result = new ArrayList<>();
-		for (String a: values) {
-			result.add(Double.parseDouble(a));
-		}
-		return result;
-	}
-	
-	private void trimDataset(){
-		//read idexes of columns to use
-		//System.out.println("No. rows before: " + dataset.size());
-		//System.out.println("No. columns before: " + columns.size());
-		columnsToUse = new ArrayList<Integer>();
-		System.out.println("Enter indexs of columns to be used for prediction in range 0 - " + numColumns);
-		Scanner s = new Scanner(System.in);
-        Scanner numScanner = new Scanner(s.nextLine()); // read indexes in one line
-        while (numScanner.hasNextInt()) {
-            columnsToUse.add(numScanner.nextInt());
-        } //columnsToUse now contains the indexes of columns to be used, predictionColumn holds one to predict
-		
-		ArrayList<ArrayList<Double>> newColumns = new ArrayList<ArrayList<Double>>();
-		for(int a: columnsToUse){
-			newColumns.add(columns.get(a));
-		} // adds only the columns needed for prediction to newColumns
-		newColumns.add(columns.get(predictionColumn)); //add predictionColumn last
-		
-		ArrayList<ArrayList<Double>> newDataset = new ArrayList<ArrayList<Double>>();
-		int columnSize = newColumns.get(0).size();
-		
-		for(int i = 0; i < columnSize; i++){ //for each row
-			ArrayList<Double> currentRow = new ArrayList<Double>();
-			for(int j = 0; j < newColumns.size(); j++){ //add each column value
-				currentRow.add(newColumns.get(j).get(i));
-			}
-			newDataset.add(currentRow);
-			/*
-			for(Double a: currentRow){
-				System.out.print(a + " ");
-			}
-			System.out.println();
-			*/
-		}
-		dataset = newDataset;
-		columns = newColumns;
-		//System.out.println("No. rows after: " + dataset.size());
-		//System.out.println("No. columns after: " + columns.size());
-	}
-	
-	private void normaliseDataset() { // all values in the range 0-1 to stop skewing by attributes such as age
-		int size = columns.size() -1; //-1 because we dont want to normalise prediction column
-		double [] maxValues = new double [size];
-		double [] minValues = new double [size]; // these will hold max and min values for each column
-		for (int i = 0; i < size; i++) {
-			maxValues[i] = Collections.max(columns.get(i));
-			minValues[i] = Collections.min(columns.get(i));
+		if(partitionTrainSet){
+			Pair<ArrayList<ArrayList<Double>>,ArrayList<ArrayList<Double>>> splits = CommonMethods.randomlyHalfSet(trainSet);
+			trainSet = splits.getFirst();
+			validationSet = splits.getSecond();
 		}
 		
-		Iterator<ArrayList<Double>> iter = dataset.iterator();
-		while (iter.hasNext()) { // loop through rows
-			ArrayList<Double> current = iter.next(); // current row
-			for (int i = 0; i < size; i++) { // loop through  rows to be normalised
-				double oldValue = current.get(i);
-				double normValue = Double.parseDouble(df.format((oldValue - minValues[i])/(maxValues[i]- minValues[i]))); // normalisation formula
-				current.set(i, normValue);
-			}
-		}
-		/*
-		for(ArrayList<Double> a : dataset){ //for each row
-			System.out.print("[");
-			for(Double x : a){
-				System.out.print(x + " ");
-			}
-			System.out.println("]");
-		}
-		*/
-	}
-	
-	private void classifyDataset() { // this sets up classes in training set, adds new column, if needed for continuous sets
-		if(splitPoints.length > 0){
-			Iterator<ArrayList<Double>> iter4 = dataset.iterator();
-			while (iter4.hasNext()) { // loop through rows
-				ArrayList<Double> current = iter4.next(); // current row
-				Double value = current.get(current.size() -1); //last row is predictionColumn since trim
-				int classCount = 1;
-				boolean assigned = false;
-				loop:
-				for(int i = 0; i < splitPoints.length; i++){
-					if(value < splitPoints[i]){
-						//class = classCount
-						current.add(new Double(classCount));
-						assigned = true;
-						break loop;
-					}
-					classCount++;
-				}
-				if(assigned == false){
-					//class = classCount
-					current.add(new Double(classCount));
-					assigned = true;
-				}
-				//System.out.println(value + ": " + classCount );
-			}
-		}
-		/*
-		for(ArrayList<Double> a : dataset){ //for each row
-			System.out.print("[");
-			for(Double x : a){
-				System.out.print(x + " ");
-			}
-			System.out.println("]");
-		}
-		*/
-	}
-	
-	private void predictClassKNN() { // train on first 200, run on second 195
-		HashSet<ArrayList<Double>> trainingSet = new HashSet<>();
-		HashSet<ArrayList<Double>> validationSet = new HashSet<>();
-		Iterator<ArrayList<Double>> iter5 = dataset.iterator(); // splits into two sets
-        int rnd;
-		while (iter5.hasNext()) { // loop through rows
-			rnd = ThreadLocalRandom.current().nextInt(1, 11);
-			if (rnd <= 5)
-				trainingSet.add(iter5.next()); // 200 rows
-			else
-				validationSet.add(iter5.next());// 195
-		}
-		HashMap<ArrayList<Double>,Double> resultsMap = new HashMap<>(); // this will hold 195 (row, predictedClass) prediction pairs.
-		
+		HashMap<ArrayList<Double>,Double> resultsMap = new HashMap<>(); // this will hold (row, predictedClass) prediction pairs.
 		Iterator<ArrayList<Double>> iter6 = validationSet.iterator();
+		
+		int o =0;
 		while (iter6.hasNext()) { // classify validation set one by one using knn and add to resultsMap
-			TreeMap<Double,Double> distanceMap = new TreeMap<Double,Double>(); // this will hold 200(dist, class) pairs. TreeMap keep them in sorted order - can use pollFirstEntry
+		
+			int currentK = k;
 			ArrayList<Double> current = iter6.next(); // current validation row
-			Iterator<ArrayList<Double>> iter7 = trainingSet.iterator(); // calculate distance from this to each row in training set
-			while (iter7.hasNext()) {
-				ArrayList<Double> trainRow = iter7.next();
-				// System.out.println(trainRow.get(33));
-				double dist = weightedEuclideanDistance(current, trainRow);
-				// System.out.println(dist);
-				distanceMap.put(dist, trainRow.get(33)); //  index 33 holds clasification value
-			}
-
-			int [] classCount = new int[4]; // we wont use index 0.
 			
-			for (int k = 0; k < 5; k++) { // 5 nearest neighbours
-				Map.Entry<Double,Double> a = distanceMap.pollFirstEntry(); // returns and removes smallest key entry in treemap - closest neighbour
-				classCount[Integer.valueOf(a.getValue().intValue())]++;  // converts double to int and increments index 1,2 or 3 of classCount
-				// System.out.print("(" + a.getKey() + "," + a.getValue() + ") ");
+			TreeMap<Double,ArrayList<Double>> distanceMap = genDistanceMap(current, trainSet);
+			
+			TreeMap<Double,ArrayList<Double>> neighbourMap = getKClosest(distanceMap, currentK);
+			
+			HashMap<Double,Double> classCounts = getClassCounts(neighbourMap);
+			
+			while(!isOneMax(classCounts)){ //in event of tie reduce k by 1 and re-run
+				currentK--;
+				neighbourMap = getKClosest(neighbourMap, currentK);
+				classCounts = getClassCounts(neighbourMap);
 			}
+			
+			Double classValue = getMaxClass(classCounts);
+			if(o == 50){
+				for(Double a: classCounts.keySet())
+					System.out.println(a + ": " + classCounts.get(a));
+				System.out.println(classValue);
+			}
+			
+			resultsMap.put(current, classValue);
+			//assign resulting class
+			
+				
+			
+			o++;
+				
+		}
+			/*
+			 
+			
+			
+			
 			
 			if (!isOneMax(classCount)) { // in the event of 2 way tie(2,2,1) - increase to 6 nearest neighbours
 				Map.Entry<Double,Double> a = distanceMap.pollFirstEntry();
@@ -262,7 +108,8 @@ public class KNN{
 				resultsMap.put(current, 3.0);
 		}
 		
-		// next step is check success rate, compare prediction with actual
+		// next step is check success rate, compa
+		re prediction with actual
 		double rightCount = 0;
 		int g = 0;
 		Iterator it = resultsMap.entrySet().iterator();
@@ -277,14 +124,138 @@ public class KNN{
 		System.out.println(rightCount + " predictions out of 195 correct");
 		System.out.println(success + "% of predictions correct");
 		// System.out.println("total " + g);
+		*/
 	}
 	
+	private static HashMap<Double, Double> getClassCounts(TreeMap<Double,ArrayList<Double>> neighbourMap){
+		HashMap<Double, Double> classCounts = new HashMap<Double, Double>();
+		for(Double x: neighbourMap.keySet()){ //distances
+			ArrayList<Double> current = neighbourMap.get(x); // class values associated with distance x
+			for(Double a: current){ //for each class value associated with current dist
+				if(classCounts.containsKey(a)){
+					Double count = classCounts.get(a);
+					count++;
+					classCounts.put(a, count);
+				}
+				else{
+					classCounts.put(a, 1.0);
+				}
+			}
+		}
+		return classCounts;
+	}
+	
+	private static double weightedEuclideanDistance(ArrayList<Double> a, ArrayList<Double> b){ 
+		DecimalFormat df = new DecimalFormat("#.####");
+		double total = 0;
+		for(int i = 0; i < a.size() -2; i++){  /////////////////////////////this needs to be fixed, at what stage in the program will partition take place? not in this method, needs to happen at start
+			double difference = (a.get(i) - b.get(i));
+			total += (1 * difference * difference); //replace 1 with weighting scheme
+		}
+		return Double.parseDouble(df.format(Math.sqrt(total)));
+	}
+	
+	private static ArrayList<Double> getNRandomListDoubles(ArrayList<Double> inputList, int n){
+		ArrayList<Double> input = new ArrayList<Double>(inputList); //because arraylists passed by reference
+		ArrayList<Double> output = new ArrayList<Double>();
+		for(int i = 0; i < n; i++){
+			int rnd = ThreadLocalRandom.current().nextInt(0, input.size());
+			output.add(input.get(rnd));
+			input.remove(rnd);
+		}
+		return output;
+	}
+	
+	private static TreeMap<Double,ArrayList<Double>> getKClosest(TreeMap<Double,ArrayList<Double>> input, int k){
+		TreeMap<Double,ArrayList<Double>> distanceMap = new TreeMap<Double,ArrayList<Double>>(input); //because of pass by reference
+		TreeMap<Double,ArrayList<Double>> neighbourMap = new TreeMap<Double,ArrayList<Double>>();
+		int j = 0;
+		while (j < k) { // k nearest neighbours
+			Map.Entry<Double,ArrayList<Double>> a = distanceMap.pollFirstEntry(); // returns and removes smallest key entry in treemap - closest neighbour    (Store these in structure, method to get dominant one)
+			int q = a.getValue().size();
+					
+			if((q + j) > k){
+				int n = k - j; //choose n from q;
+				ArrayList<Double> newA = getNRandomListDoubles(a.getValue(), n);
+				neighbourMap.put(a.getKey(),newA);
+				j = j + n;
+			}
+			else{
+				neighbourMap.put(a.getKey(),a.getValue());
+				j = j + q;
+			}
+		}
+		return neighbourMap;
+	}
+	
+	private static TreeMap<Double,ArrayList<Double>> genDistanceMap(ArrayList<Double> valRow, ArrayList<ArrayList<Double>> trainSet) {
+		TreeMap<Double,ArrayList<Double>> distanceMap = new TreeMap<Double,ArrayList<Double>>(); // this will hold (dist, classes) pairs. TreeMap keeps them in sorted order - can use pollFirstEntry
+		Iterator<ArrayList<Double>> iter7 = trainSet.iterator(); // calculate distance from this to each row in training set
+		while (iter7.hasNext()) {
+			ArrayList<Double> trainRow = iter7.next();
+				
+			double dist = weightedEuclideanDistance(valRow, trainRow);
+			if(distanceMap.containsKey(dist)){
+				ArrayList<Double> t = distanceMap.get(dist);
+				t.add(trainRow.get(trainRow.size() -1));
+				distanceMap.put(dist, t);
+			}
+			else{
+				ArrayList<Double> b = new ArrayList<Double>();
+				b.add(trainRow.get(trainRow.size() -1));
+				distanceMap.put(dist, b); //  last index holds class value of training set
+			}
+		}
+		return distanceMap;
+	}
+	
+	
+	private static boolean isOneMax(HashMap<Double,Double> classCounts){
+		//sort 
+		//if next index the same as first then there is at least two max therefore return false
+		if(classCounts.keySet().size() == 1){
+				return true;
+		}
+		else{
+			ArrayList<Double> a = new ArrayList<Double>();
+			for(Double x: classCounts.keySet())
+				a.add(classCounts.get(x));
+			Collections.sort(a);
+			Collections.reverse(a); //now in desc order
+			if(a.get(0).equals(a.get(1))){
+				return false;
+			}
+			else{
+				return true;
+			}
+		}
+	}
+	
+	private static Double getMaxClass(HashMap<Double,Double> classCounts){
+		//create tree map with comparitor which sorts by value instead of key
+		TreeSet<Map.Entry<Double, Double>> entriesSet = new TreeSet<>(new Comparator<Map.Entry<Double, Double>>(){
+           @Override 
+			public int compare(Map.Entry<Double, Double> me1, Map.Entry<Double, Double> me2) {
+				return me1.getValue().compareTo(me2.getValue());
+			}
+        });
+        entriesSet.addAll(classCounts.entrySet());
+		return entriesSet.last().getKey();
+	}
+	
+
 	public static void main(String [] args) {
-		KNN z = new KNN("student-mat-normalised.csv", true, 33, 32, new int[]{5,10,15});
-		z.readDataset();
-		z.trimDataset();
-		z.normaliseDataset();
-		z.classifyDataset();
+		//KNN z = new KNN("student-mat-normalised.csv", true, true, 33, 32, new int[]{5,10,15});
+		ArrayList<ArrayList<Double>> trainSet = CommonMethods.readDatasetFile("student-mat-normalised.csv", true);
+		ArrayList<ArrayList<Double>> trainColumns = CommonMethods.createColumnStructure(trainSet);
+		trainColumns = CommonMethods.trimColumns(trainColumns, new int[]{3,5,7,16,22,28}, 32);
+		trainSet = CommonMethods.createRowStructure(trainColumns);
+		trainSet = CommonMethods.normaliseDataset(trainSet, trainColumns);
+		trainSet = CommonMethods.classifyDataset(trainSet, new int[]{5,10,15});
+		trainColumns = CommonMethods.createColumnStructure(trainSet);
+		predictClassKNN(true, trainSet, null, 5);
+		
+		
 	}
 	
 }
