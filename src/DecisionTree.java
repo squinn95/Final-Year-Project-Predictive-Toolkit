aff353ca83febc76b51d0783e51878b9a5536955
Sean Quinn <sean.quinn34@mail.dcu.ie>
2017-02-28 00:01:48 +0000

    Upload new file

---
+++ src/DecisionTree.java
@@ -0,0 +1,70 @@
+//package prediction;
+import java.io.*;
+import java.util.*;
+import java.text.DecimalFormat;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Bayes{
+	
+	private static void predictClassBayes(boolean partitionTrainSet, String trainSetPath, String validationSetPath, boolean headings, int [] columnsToUse, int predictionColumn, int [] splitPoints) {
+		DecimalFormat df = new DecimalFormat("#.####");
+		// read train
+		ArrayList<ArrayList<Double>> trainSet = CommonMethods.readDatasetFile(trainSetPath, headings);
+		ArrayList<ArrayList<Double>> validationSet;
+		//split train set into two if needed
+		if(partitionTrainSet){
+			Pair<ArrayList<ArrayList<Double>>,ArrayList<ArrayList<Double>>> splits = CommonMethods.randomlyHalfSet(trainSet);
+			trainSet = splits.getFirst();
+			validationSet = splits.getSecond();
+		}
+		else{
+			validationSet = CommonMethods.readDatasetFile(validationSetPath, headings);
+		}
+		
+		//create column structure
+		ArrayList<ArrayList<Double>> trainColumns = CommonMethods.createColumnStructure(trainSet);
+		ArrayList<ArrayList<Double>> validationColumns = CommonMethods.createColumnStructure(validationSet);
+		
+		//trim columns which wont be used
+		trainColumns = CommonMethods.trimColumns(trainColumns, columnsToUse, predictionColumn);
+		validationColumns = CommonMethods.trimColumns(validationColumns, columnsToUse, predictionColumn);
+		
+		//convert trimmed columns back to row structure
+		trainSet = CommonMethods.createRowStructure(trainColumns);
+		validationSet = CommonMethods.createRowStructure(validationColumns);
+		
+		//split classification column into sets if needed, for continuous variables
+		trainSet = CommonMethods.classifyDataset(trainSet, splitPoints);
+		validationSet = CommonMethods.classifyDataset(validationSet, splitPoints);
+		
+		
+		double rightCount = 0;
+		int g = 0;
+		Iterator<ArrayList<Double>> iter6 = validationSet.iterator();
+		
+		while (iter6.hasNext()) { // classify validation set one by one
+		
+			ArrayList<Double> currentRow = iter6.next(); // current validation row
+			
+			//classify and increment right count
+
+			g++;
+		}
+		
+		double success = Double.parseDouble(df.format((rightCount/g)*100)); // percentage success rounded to 4 places
+		System.out.println(rightCount + " predictions out of " + g + " correct");
+		System.out.println("Bayes classifier built with " + success + "% accuracy");
+	
+	}
+	
+	
+	public static void main(String [] args) {
+	
+		//predictClassBayes(true, "student-mat-normalised.csv", null, true, new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18},32, new int[]{7,14});
+		//predictClassBayes(true, "IrisNumerical.csv", null, true, new int[]{1,2,3,4},5, null);
+		predictClassBayes(true, "CarDataNumerical.csv", null, true, new int[]{0,1,2,3,4,5},6, null);
+		
+		
+	}
+	
+}
