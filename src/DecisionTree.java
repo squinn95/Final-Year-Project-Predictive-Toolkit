fcdc2d400b8157558a53529be2bca96881bd18e2
Sean Quinn <sean.quinn34@mail.dcu.ie>
2017-03-11 20:34:19 +0000

    Replace DecisionTree.java

--- src/DecisionTree.java
+++ src/DecisionTree.java
@@ -42,41 +42,58 @@ public class DecisionTree{
 		trainColumns = CommonMethods.createColumnStructure(trainSet);
 		validationColumns = CommonMethods.createColumnStructure(validationSet);
 		
-		/*
-		for(int i =0; i < trainColumns.size() -1; i++){
-			//System.out.println(i + ": " + infoGain(trainColumns,i));
-			gainRatio(trainColumns,i);
-		*/
-		
 		ArrayList<Integer> usedAtts = new ArrayList<Integer>();
 		
 		Node root = new Node(trainColumns, usedAtts);
 		
-		buildTree(root, "Information Gain");
+		buildTree(root, formula);
+
+		//preOrderPrint(root,0);
 		
-		preOrder(root);
+		Iterator<ArrayList<Double>> iter6 = validationSet.iterator();
+		double rightCount = 0;
+		int g = 0;
 		
+		while (iter6.hasNext()) { // classify validation set one by one using tree and add to resultsMap
+			ArrayList<Double> current = iter6.next();
+			double predictedClass = traverseTree(current, root);
+			if(CommonMethods.equalsDouble(current.get(current.size()-1),predictedClass))
+				rightCount++;
+			g++;
+		}
+		
+		double success = Double.parseDouble(df.format((rightCount/g)*100)); // percentage success rounded to 4 places
+		System.out.println(rightCount + " predictions out of " + g + " correct");
+		System.out.println("Decision tree classifier built with " + success + "% accuracy");
 	}
 	
-	public static void preOrder(Node root) {  
+	public static void preOrderPrint(Node root, int depth) {  
 			if(root == null)
 				return;
-				
-			System.out.println("split on: " + root.getSplitIndex() + ", atts used: " + root.getUsedAttributes());
 			
-			if(root.isLeaf())
-				return;
-			
-			for(Double x: root.children.keySet()){
-				preOrder(root.children.get(x));
+			for(int i = 0; i < depth; i++){
+				System.out.print("  ");
 			}
+			System.out.print(root.getSplitIndex());
 			
+			if(root.isLeaf()){
+				System.out.println("{}");
+				return;
+			}
+			int newDepth = depth + 1;
+			System.out.println("{");
+			for(Double x: root.children.keySet()){
+				preOrderPrint(root.children.get(x),newDepth);
+			}
+			for(int i = 0; i < depth; i++){
+				System.out.print("  ");
+			}
+			System.out.println("}");
 	}
-	
+		
 	static double infoGain(ArrayList<ArrayList<Double>> trainColumns, int targetColumnIndex){ //pred column last index
 	
 		HashMap<Double,ArrayList<Integer>> classIndexMap = CommonMethods.getColumnValueLocationMap(trainColumns, trainColumns.size()-1);  //hash map: key is class, value is arraylist of indexes
-		
 		double setEntropy = getEntropy(trainColumns, trainColumns.size()-1);
 		double setSize = (double)trainColumns.get(0).size();
 		
@@ -86,7 +103,6 @@ public class DecisionTree{
 		for(Double currentColumnValue: columnValueIndexMap.keySet()){
 			double valueCount = (double)columnValueIndexMap.get(currentColumnValue).size();
 			double valueRatio = (valueCount/setSize);
-			
 			double accum = 0.0;
 			for(Double currentClass: classIndexMap.keySet()){
 				double commonCount = 0.0;
@@ -95,25 +111,17 @@ public class DecisionTree{
 						commonCount++;
 					}
 				}
-			
 				double ratio = (commonCount/valueCount);
 				double calc;
 				if (ratio > 0.0)
 					calc = (-1 * ratio * (Math.log(ratio)/Math.log(2)));
 				else
 					calc = 0.0;
-				
 				accum += calc;
-				//System.out.println("columnValue: " + currentColumnValue + ", currentColumnValue size: " + valueCount + ", class: " + currentClass + ", count: " + commonCount + ", calc: " + calc);
 			}
 			columnEntropy += (valueRatio * accum);
-			//System.out.println();
 		}
-		
-		//System.out.println("Set entropy: " +setEntropy + ", Column entropy: " + columnEntropy);
-		
-		double gain = (setEntropy - columnEntropy);
-		
+		double gain = (setEntropy - columnEntropy);		
 		return gain;
 	}
 	
@@ -127,102 +135,52 @@ public class DecisionTree{
 		for(Double currentClass: columnValueIndexMap.keySet()){
 			double size = (double)columnValueIndexMap.get(currentClass).size();
 			double ratio = (size/setSize);
-			double calc;
-				if (ratio > 0.0)
-					calc = (-1 * ratio * (Math.log(ratio)/Math.log(2)));
-				else
-					calc = 0.0;
+			double calc = (-1 * ratio * (Math.log(ratio)/Math.log(2)));
 			splitInfo += calc;
 		}
-		
-		double gainRatio = (infoGain/splitInfo);
-		//System.out.println("info gain: " + infoGain + ", gainRatio: " + gainRatio);
+		double gainRatio;
+		if(infoGain != 0.0)
+			gainRatio = (infoGain/splitInfo);
+		else
+			gainRatio = 0.0;
 		return gainRatio;
-		
 	}
 	
 	static double getEntropy(ArrayList<ArrayList<Double>> trainColumns, int columnIndex){
 		HashMap<Double,ArrayList<Integer>> classIndexMap = CommonMethods.getColumnValueLocationMap(trainColumns, columnIndex);  //hash map: key is class, value is arraylist of indexes
-		
 		double setEntropy = 0.0;
 		double setSize = (double)trainColumns.get(columnIndex).size();
 		for(Double currentClass: classIndexMap.keySet()){
 			double size = (double)classIndexMap.get(currentClass).size();
 			double ratio = (size/setSize);
-			double calc;
-				if (ratio > 0.0){
-					calc = (-1 * ratio * (Math.log(ratio)/Math.log(2)));
-					/*
-					if(calc == 0.0){
-						System.out.println("proper 0 entropy");
-					}
-					*/
-				}
-				else
-					System.out.println("Problem - should never be in here");
-					calc = 0.0;
+			double calc = (-1 * ratio * (Math.log(ratio)/Math.log(2)));
 			setEntropy += calc;
-			//System.out.println("class: " + currentClass + ", size: " + size + "/" + setSize + ", ratio: " + ratio + ", Entropy: " + calc);
 		}
 		return setEntropy;
 	}
 	
-
+	static double gainFormula(ArrayList<ArrayList<Double>> trainColumns, int targetColumnIndex, String formula){
+		if(formula.equals("GainRatio")){
+			return gainRatio(trainColumns,targetColumnIndex);
+		}
+		else{ //information gain
+			return infoGain(trainColumns,targetColumnIndex);
+		}
+	}
+	
 	static int selectAttribute(ArrayList<ArrayList<Double>> trainColumns, String formula, ArrayList<Integer> usedAttributes){ //returns index of attribute to be used for next split
-		
-			if(formula.equals("Gain Ratio")){
-				int bestIndex = -1;
-				double bestGR = 0.0;
-				for(int i = 0; i < trainColumns.size()-1; i++){ //last element is pred column
-					if(!usedAttributes.contains(i)){
-						double currentGR = gainRatio(trainColumns,i);
-						//System.out.print("row: " + i + ", GR: " + currentGR + " /// ");
-						if(currentGR > bestGR){
-							bestGR = currentGR;
-							bestIndex = i;
-						}
-					}
-					
+		int bestIndex = -1;
+		double bestGain = 0.0;
+		for(int i = 0; i < trainColumns.size()-1; i++){ //last element is pred column
+			if(!usedAttributes.contains(i)){
+				double currentGain = gainFormula(trainColumns,i,formula);
+				if(currentGain >= bestGain){
+					bestGain = currentGain;
+					bestIndex = i;
 				}
-				//System.out.println();
-				return bestIndex;
-			}
-			else{ //information gain
-				int bestIndex = -1;
-				double bestIG = 0.0;
-				for(int i = 0; i < trainColumns.size()-1; i++){ //last element is pred column
-					if(!usedAttributes.contains(i)){
-						double currentIG = infoGain(trainColumns,i);
-						if(currentIG > bestIG){
-							bestIG = currentIG;
-							bestIndex = i;
-						}
-					}
-	
-				}
-				return bestIndex;
-			}
-	}
-	
-	private static Double getMaxCount(HashMap<Double,Double> classCounts){
-		//create tree map with comparitor which sorts by value instead of key
-		TreeSet<Map.Entry<Double, Double>> entriesSet = new TreeSet<>(new Comparator<Map.Entry<Double, Double>>(){
-           @Override 
-			public int compare(Map.Entry<Double, Double> x, Map.Entry<Double, Double> y) {
-				return x.getValue().compareTo(y.getValue());
-			}
-        });
-        entriesSet.addAll(classCounts.entrySet());
-		return entriesSet.last().getKey();
-	}
-	
-	public static void main(String [] args) {
-	
-		//predictClassBayes(true, "student-mat-normalised.csv", null, true, new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18},32, new int[]{7,14});
-		//predictClassBayes(true, "IrisNumerical.csv", null, true, new int[]{1,2,3,4},5, null);
-		predictClassDecisionTree(true, "CarDataNumerical.csv", null, true, new int[]{0,1,2,3,4,5},6, null, "Gain Ratio");
-		
-		
+			}		
+		}
+		return bestIndex;
 	}
 	
 	static Node buildTree(Node root, String formula) {
@@ -241,7 +199,6 @@ public class DecisionTree{
 				root.setLeaf(true);
 				return root;
 			}
-		
 			
 		//if not - split & get children and call on them
 		
@@ -249,15 +206,15 @@ public class DecisionTree{
 		
 		HashMap<Double,ArrayList<Integer>> columnValueIndexMap = CommonMethods.getColumnValueLocationMap(currentColumns, bestAttribute);
 		
-		HashMap<Double,ArrayList<ArrayList<Double>>> classColumnStructuresMap = CommonMethods.getClassColumnStructuresMap(CommonMethods.createRowStructure(currentColumns), columnValueIndexMap);
+		HashMap<Double,ArrayList<ArrayList<Double>>> indexColumnStructuresMap = CommonMethods.getIndexColumnStructuresMap(CommonMethods.createRowStructure(currentColumns), columnValueIndexMap);
 		
 		root.setSplitIndex(bestAttribute);
 		
 		ArrayList<Integer> atts = root.getUsedAttributes();
 		atts.add(bestAttribute);
 		
-		for (Double a: classColumnStructuresMap.keySet()) {
-			Node currentChild = new Node(classColumnStructuresMap.get(a),atts);
+		for (Double a: indexColumnStructuresMap.keySet()) {
+			Node currentChild = new Node(indexColumnStructuresMap.get(a),atts);
 			root.children.put(a,currentChild);
 		}
 		
@@ -265,12 +222,40 @@ public class DecisionTree{
 			buildTree(root.children.get(x),formula);
 		}
 		
-		//return
-		
-		return root;
-		
+		return root;	
 	}
 	
+	static Double traverseTree(ArrayList<Double> r, Node root) {
+		
+		if(!root.isLeaf()){ //stop if current node is leaf
+			double nodeValue = r.get(root.getSplitIndex()); //the value of the index this node splits on
+			
+			if(root.children.containsKey(nodeValue)){
+				return traverseTree(r,root.children.get(nodeValue));
+			}
+			 //if false -this means this value is not in children - so stop at current node
+		}
+	
+		//tree has been traversed, get majority count of root data
+		ArrayList<ArrayList<Double>> resultTrainColumns = root.getData();
+		HashMap<Double,ArrayList<Integer>> columnValueIndexMap = CommonMethods.getColumnValueLocationMap(resultTrainColumns, (resultTrainColumns.size()-1));
+		HashMap<Double,Double> classCounts = new HashMap<Double,Double>();
+		for(Double x: columnValueIndexMap.keySet()){
+			double size = (double)columnValueIndexMap.get(x).size();
+			classCounts.put(x,size);
+		}
+		double maxClass = CommonMethods.getMaxCount(classCounts);
+		return maxClass;
+	}
+	
+	public static void main(String [] args) {
+	
+		//predictClassDecisionTree(true, "student-mat-normalised.csv", null, true, new int[]{0,1,15,17,18,26},32, new int[]{7,14},"GainRatio");
+		//predictClassDecisionTree(true, "IrisNumerical.csv", null, true, new int[]{1,2,3,4},5, null, "Gain Ratio");
+		predictClassDecisionTree(true, "CarDataNumerical.csv", null, true, new int[]{0,1,2,3,4,5},6, null, "GainRatio");
+		//predictClassDecisionTree(true, "CarDataNumerical.csv", null, true, new int[]{0,1,2,3,4,5},6, null, "Information Gain");
+		
+	}	
 }
 
 class Node {
@@ -281,8 +266,10 @@ class Node {
 	boolean isLeaf;
 	
 	public Node(ArrayList<ArrayList<Double>> trainColumns, ArrayList<Integer> usedAttributes) {
+		ArrayList<Integer> copy = new ArrayList<Integer>(usedAttributes); //to stop pass by reference
+		children = new HashMap<Double, Node>();
 		this.trainColumns = trainColumns;
-		this.usedAttributes = usedAttributes;
+		this.usedAttributes = copy;
 		setSplitIndex(-1);
 		setLeaf(false);
 	}
