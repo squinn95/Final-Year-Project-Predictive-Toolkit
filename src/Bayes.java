2b5ac59bda16950c132a955b2f0b9e507ab1df17
Sean Quinn <sean.quinn34@mail.dcu.ie>
2017-03-13 18:39:48 +0000

    Replace Bayes.java

--- src/Bayes.java
+++ src/Bayes.java
@@ -87,8 +87,8 @@ public class Bayes{
 	}	
 	
 	public static void main(String [] args) {
-		//predictClassBayes(true, "student-mat-normalised.csv", null, true, new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18},32, new int[]{7,14});
-		//predictClassBayes(true, "IrisNumerical.csv", null, true, new int[]{1,2,3,4},5, null);
+		predictClassBayes(true, "student-mat-normalised.csv", null, true, new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18},32, new int[]{7,14});
+		predictClassBayes(true, "IrisNumerical.csv", null, true, new int[]{1,2,3,4},5, null);
 		predictClassBayes(true, "CarDataNumerical.csv", null, true, new int[]{0,1,2,3,4,5},6, null);	
 	}
 }
